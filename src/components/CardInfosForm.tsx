472ee2eccb6dbdf2976d2d0d28355b06f0539424
Gabriel Diniz <gabrielpombodiniz@gmail.com>
2023-04-27 14:40:32 -0300

    feat: finishing

--- src/components/CardInfosForm.tsx
+++ src/components/CardInfosForm.tsx
@@ -4,18 +4,69 @@ import CardInfoInput from "./CardInfoInput";
 export default function CardInfosForm() {
   return (
     <form className="w-full max-w-sm flex flex-col gap-5 mb-5">
-      {inputLabels.map(({ label, placeholder }) => (
-        <CardInfoInput key={label} label={label} placeholder={placeholder} />
-      ))}
+      <label className="relative">
+        <div className="absolute ml-5 -top-[30%] bg-white border-2 border-white text-label-text">
+          Nome completo
+        </div>
+        <input
+          type="text"
+          placeholder="João Linaldo Dias Fraga Santos"
+          className={
+            "w-full p-2 pl-4 border-2 border-gray rounded-xl transition-colors focus:border-green focus:outline-none"
+          }
+        />
+      </label>
+      <label className="relative">
+        <div className="absolute ml-5 -top-[30%] bg-white border-2 border-white text-label-text">
+          CPF
+        </div>
+        <input
+          type="text"
+          placeholder="405.503.503-15"
+          className={
+            "w-full p-2 pl-4 border-2 border-gray rounded-xl transition-colors focus:border-green focus:outline-none"
+          }
+        />
+      </label>
+      <label className="relative">
+        <div className="absolute ml-5 -top-[30%] bg-white border-2 border-white text-label-text">
+          Número do cartão
+        </div>
+        <input
+          type="text"
+          maxLength={16 }
+          placeholder="1234 5678 9101 1121"
+          className={
+            "w-full p-2 pl-4 border-2 border-gray rounded-xl transition-colors focus:border-green focus:outline-none"
+          }
+        />
+      </label>
 
       <div className="grid grid-cols-2 gap-5">
-        {smallerInputLabels.map(({ label, placeholder }) => (
-          <CardInfoInput
-            key={label}
-            label={label}
-            placeholder={placeholder}
+        <label className="relative">
+          <div className="absolute ml-5 -top-[30%] bg-white border-2 border-white text-label-text">
+            Vencimento
+          </div>
+          <input
+            type="text"
+            placeholder="10/02"
+            className={
+              "w-full p-2 pl-4 border-2 border-gray rounded-xl transition-colors focus:border-green focus:outline-none"
+            }
           />
-        ))}
+        </label>
+        <label className="relative">
+          <div className="absolute ml-5 -top-[30%] bg-white border-2 border-white text-label-text">
+            CVV
+          </div>
+          <input
+            type="text"
+            placeholder="123"
+            className={
+              "w-full p-2 pl-4 border-2 border-gray rounded-xl transition-colors focus:border-green focus:outline-none"
+            }
+          />
+        </label>
       </div>
 
       <button
